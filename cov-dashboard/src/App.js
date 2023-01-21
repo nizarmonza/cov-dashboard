a0c595f4b1603cab84e7b7d9c43880d2d54eb240
nizaralydrus <nizarmonzaa@gmail.com>
2023-01-21 08:52:50 -0600

    first

---
+++ cov-dashboard/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+// Imports end
+function App() {
+// App component starts here
+//return statement goes below this
+  return (
+    <div className="App">
+      <h1>COVID 19 Dashboard </h1>
+      <div className="dashboard-container">
+        <div className="dashboard-menu"></div>
+        <div className="dashboard-timeseries"></div>
+        <div className="dashboard-summary"></div>
+      </div>
+    </div>
+  );
+}
+ 
+export default App;
